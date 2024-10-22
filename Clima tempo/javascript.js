26ad46d3443875cdcb0396f8a2bdbcf167b451b1
Marcos Fagundes <marcos733tuto@gmail.com>
2024-10-22 14:13:13 -0300

    java script funcionando parcialmente
    java esta funcionando parcialmente, porem, estou com algum problema

--- Clima tempo/javascript.js
+++ Clima tempo/javascript.js
@@ -2,11 +2,13 @@ document.querySelector('#btn-reset').addEventListener('click', () => {
     const input = document.querySelector('input')
     input.value = ''
     input.focus()
-
+    
+    clearInfo()
 })
 
 document.querySelector('#search').addEventListener('click', async(event) => {
     event.preventDefault();
+    clearInfo()
 
     let input = document.querySelector('#searchInput').value
 
@@ -15,23 +17,50 @@ document.querySelector('#search').addEventListener('click', async(event) => {
 
         let json = await response.json()
 
-        console.log(json)
         if (json.cod === 200) {
             showInfo({
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
-            })
+                description: json.weather[0].description,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })} else {
+            clearInfo()
+            console.log('Não encontramos esta localização.');
         }
     } else {
-        console.log('Não encontramos esta localização.');
+        clearInfo()
     }
+
+    cler
 })
 
 function showInfo(obj) {
-    console.log(`${obj.name}`)
-    console.log(`${obj.country}`)
-    console.log(`${obj.temp}`)
-    console.log(`${obj.description}`)
+    showWarning('')
+
+    document.querySelector('.city').innerHTML = `${obj.name}, ${obj.country}`.toUpperCase()
+
+    document.querySelector('.celsius').innerHTML = `${obj.temp} <sup>°C</sup>`
+
+    document.querySelector('.weatherImg').innerHTML = `<img src="http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png"></img>` 
+
+    document.querySelector('.weatherImg').innerHTML += `<figure style="font-size: 0.9rem;"><i>${obj.description}</i></figure>`
+
+    document.querySelector('.windInfo').innerHTML = `${obj.windSpeed} <sup>km/h</sup>`
+
+    document.querySelector('.windDot').style.transform += `rotate(${obj.windAngle-90}deg)`
+
+    document.querySelector('.weather').style.display = 'block'
+
 }
 
+function clearInfo() {
+    showWarning('')
+    document.querySelector('.weather').style.display = 'none'
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
+}
