f8231ec9e9d51084b58090b6163b9f28a9ad55ce
Marcos Fagundes <marcos733tuto@gmail.com>
2024-10-22 15:45:03 -0300

    end mobile first
    terminei o layout mobile, agr falta os ajuste de desktop

--- Clima tempo/javascript.js
+++ Clima tempo/javascript.js
@@ -13,6 +13,9 @@ document.querySelector('#search').addEventListener('click', async(event) => {
     let input = document.querySelector('#searchInput').value
 
     if (input !== '') {
+        clearInfo()
+        showWarning('Carregando')
+
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`)
 
         let json = await response.json()
@@ -27,18 +30,20 @@ document.querySelector('#search').addEventListener('click', async(event) => {
                 windSpeed: json.wind.speed,
                 windAngle: json.wind.deg
             })} else {
-            // clearInfo()
+            clearInfo()
             console.log('Não encontramos esta localização.');
         }
     } else {
-        // clearInfo()
+        clearInfo()
+        showWarning('Não encontramos esta localização.')
     }
 
-    // clearInfo()
 })
 
 
 function showInfo(obj) {
+    showWarning('')
+
     document.querySelector('.city').innerHTML = `${obj.name}, ${obj.country}.`.toLocaleUpperCase()
 
     document.querySelector('.celsius').innerHTML = `${obj.temp} <sup>°C</sup>`
@@ -47,5 +52,15 @@ function showInfo(obj) {
     document.querySelector('.weatherImg img').setAttribute('src', 
         `http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png`)
     
-    document.querySelector('.windDot').style.transform = `rotate(${obj.windAngle-90}deg)`
+    document.querySelector('.windDot').style.transform = `rotate(${obj.windAngle - 90}deg)`
+    
+    document.querySelector('.result').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('')
+    document.querySelector('.result').style.display = 'none'
+}
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
 }
