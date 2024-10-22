2780b8004615cfba1011cdc6f35894eeb36be38e
Marcos Fagundes <marcos733tuto@gmail.com>
2024-10-22 14:40:58 -0300

    ajustes

--- Clima tempo/javascript.js
+++ Clima tempo/javascript.js
@@ -34,33 +34,5 @@ document.querySelector('#search').addEventListener('click', async(event) => {
         clearInfo()
     }
 
-    cler
+    clearInfo()
 })
-
-function showInfo(obj) {
-    showWarning('')
-
-    document.querySelector('.city').innerHTML = `${obj.name}, ${obj.country}`.toUpperCase()
-
-    document.querySelector('.celsius').innerHTML = `${obj.temp} <sup>°C</sup>`
-
-    document.querySelector('.weatherImg').innerHTML = `<img src="http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png"></img>` 
-
-    document.querySelector('.weatherImg').innerHTML += `<figure style="font-size: 0.9rem;"><i>${obj.description}</i></figure>`
-
-    document.querySelector('.windInfo').innerHTML = `${obj.windSpeed} <sup>km/h</sup>`
-
-    document.querySelector('.windDot').style.transform += `rotate(${obj.windAngle-90}deg)`
-
-    document.querySelector('.weather').style.display = 'block'
-
-}
-
-function clearInfo() {
-    showWarning('')
-    document.querySelector('.weather').style.display = 'none'
-}
-
-function showWarning(msg) {
-    document.querySelector('.aviso').innerHTML = msg
-}
